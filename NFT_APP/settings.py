e3c604f0d5f949363b34c71633c5d4e6e6b55538
Daiki Ichikawa <71591906+ka1ch0@users.noreply.github.com>
2021-12-02 00:06:55 +0900

    Update settings.py
    fix secret key

--- NFT_APP/settings.py
+++ NFT_APP/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+-n0(5d%ls+o*sc&=*5_y79-u!q92@$56v!k9v_xf-w8fra&gg'
+SECRET_KEY = 'your key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
